8be25da3bb8533891e059ee6cde4a7ded6b73924
RafalKaczynski0 <rafal.kaczynski@wir.pl>
2025-02-01 17:29:22 +0100

    Completed

--- Program.cs
+++ Program.cs
@@ -1,15 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Projekt
+class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Podaj początek:");
+        int start = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Podaj koniec:");
+        int end = int.Parse(Console.ReadLine());
+
+        while (start >= end) 
         {
+            Console.WriteLine("Początek musi być mniejszy od końca, podaj jeszcze raz");
+
+            Console.WriteLine("Podaj początek:");
+            start = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj koniec:");
+            end = int.Parse(Console.ReadLine());
         }
+
+        Random rnd = new Random();
+        int randomNumber = rnd.Next(start, end + 1);
+
+        Console.WriteLine("Wylosowano liczbę. Spróbuj ją odgadnąć!");
+
+        int tries = 5;
+
+        for (int i = 0; i < tries; i++)
+        {
+            Console.WriteLine($"Próba {i + 1} z {tries}: Podaj liczbę:");
+            int userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber == randomNumber)
+            {
+                Console.WriteLine("Gratulacje! Odgadłeś/aś liczbę.");
+                return;
+            }
+            else if (userNumber > randomNumber && userNumber <= end)
+            {
+                Console.WriteLine("Twoja liczba jest większa od wylosowanej.");
+            }
+            else if (userNumber < randomNumber && userNumber >= start)
+            {
+                Console.WriteLine("Twoja liczba jest mniejsza od wylosowanej.");
+            }
+            else if (userNumber < start || userNumber > end)
+            {
+                Console.WriteLine($"Wylosuj liczbę z zakresu od {start} do {end}");
+                i--;
+            }
+            
+
+        }
+
+        Console.WriteLine("Game over!");
+        Console.WriteLine($"Wylosowana liczba to: {randomNumber}");
     }
 }
